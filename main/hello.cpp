f8c12f19cb86dd83503f27070d62dcc3163cd205
HrishitaPatra <hrishitapatra@gmail.com>
2025-03-14 13:27:26 +0530

    Making a change

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,6 +7,6 @@ int main(void)
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this \n";
-  cout << "I am learning Jenkins and its integration \n";
+  cout << "I made a change to the file\n";
   return 0;
 }
